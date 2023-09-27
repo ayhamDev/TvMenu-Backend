21d9315bee84253aec79c68a05c6b1f3eaeb88b9
ayham <ayhamgm81@gmail.com>
2023-09-27 13:56:59 +0300

    fix env

--- utils/db.js
+++ utils/db.js
@@ -5,33 +5,42 @@ const DeviceTable = require("../Model/Device.table");
 
 // Dev env (sqlite)
 // SQLite Config
-const sql = new Sequelize({
-  dialect: "sqlite",
-  storage: "./database.sqlite",
-  logging: false,
-  define: {
-    timestamps: false,
-  },
-});
-
-//   //  Microsoft SQL Server Config
-// const sql = new Sequelize("TVmenu", process.env.DB_USER, process.env.DB_PASSWORD, {
-//   host: "localhost",
-//   dialect: "mssql",
-//   port: "1433",
-//   pool: {
-//     min: 0,
-//     max: 10,
-//     idle: 25000,
-//     acquire: 25000,
-//     requestTimeout: 30000,
-//   },
-//   dialectOptions: {
-//     options: {
-//       encrypt: false,
-//     },
+// const sql = new Sequelize({
+//   dialect: "sqlite",
+//   storage: "./database.sqlite",
+//   logging: false,
+//   define: {
+//     timestamps: false,
 //   },
 // });
+
+//   //  Microsoft SQL Server Config
+const sql = new Sequelize(
+  "tvmenutest",
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: "localhost",
+    dialect: "mssql",
+    port: "1433",
+    pool: {
+      min: 0,
+      max: 10,
+      idle: 25000,
+      acquire: 25000,
+      requestTimeout: 30000,
+    },
+    logging: false,
+    define: {
+      timestamps: false,
+    },
+    dialectOptions: {
+      options: {
+        encrypt: false,
+      },
+    },
+  }
+);
 
 module.exports.sql = sql;
 module.exports.device = DeviceTable(sql);
